4756abfda403b20900069c9eb75d8c5d31aaaeb9
DL <daniyal.liaqat@outlook.com>
2018-07-23 14:20:38 -0400

    Change .get to [] for env variables to make error messages more clear

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
-OPENSMILE_HOME = os.environ.get("OPENSMILE_HOME")
+OPENSMILE_HOME = os.environ["OPENSMILE_HOME"]
 
 # Paths to external libraries and relevant files for lexicosyntactic features
-stanford_pos_path = os.environ.get("stanford_pos_path")
-stanford_parser_path = os.environ.get("stanford_parser_path")
-lu_analyzer_path = os.environ.get("lu_analyzer_path")
-cfg_rules_path = os.environ.get("cfg_rules_path")
-path_to_dictionary = os.environ.get("path_to_dictionary")
-path_to_freq_norms = os.environ.get("path_to_freq_norms")
-path_to_image_norms = os.environ.get("path_to_image_norms")
-path_to_anew = os.environ.get("path_to_anew")
-path_to_warringer = os.environ.get("path_to_warringer")
-path_to_mpqa_lexicon = os.environ.get("path_to_mpqa_lexicon")
-path_to_rst_python = os.environ.get("path_to_rst_python")
-path_to_rst = os.environ.get("path_to_rst")
-path_to_stanford_cp = os.environ.get("path_to_stanford_cp")
-path_to_lda_model = os.environ.get("path_to_lda_model")
-path_to_lda_wordids = os.environ.get("path_to_lda_wordids")
+stanford_pos_path = os.environ["stanford_pos_path"]
+stanford_parser_path = os.environ["stanford_parser_path"]
+lu_analyzer_path = os.environ["lu_analyzer_path"]
+cfg_rules_path = os.environ["cfg_rules_path"]
+path_to_dictionary = os.environ["path_to_dictionary"]
+path_to_freq_norms = os.environ["path_to_freq_norms"]
+path_to_image_norms = os.environ["path_to_image_norms"]
+path_to_anew = os.environ["path_to_anew"]
+path_to_warringer = os.environ["path_to_warringer"]
+path_to_mpqa_lexicon = os.environ["path_to_mpqa_lexicon"]
+path_to_rst_python = os.environ["path_to_rst_python"]
+path_to_rst = os.environ["path_to_rst"]
+path_to_stanford_cp = os.environ["path_to_stanford_cp"]
+path_to_lda_model = os.environ["path_to_lda_model"]
+path_to_lda_wordids = os.environ["path_to_lda_wordids"]
